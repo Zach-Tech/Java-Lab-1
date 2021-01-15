031452a43d67fbcf21273f42c95a9938f548e716
zachc <childers.zd@gmail.com>
2021-01-14 22:48:12 -0500

    Try catch and stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,50 +15,64 @@ public class Main {
         //Using doubles in case of rates
         //Such as $9.25 or Hours such as
         // 40.50
-	    String loopResponse, nameIn;
-	    double hours, rate, totalPay;
-	    Scanner input = new Scanner(in);
+        String loopResponse, nameIn;
+        double hours, rate, totalPay;
+        Scanner input = new Scanner(in);
 
-	    //Begin program loop:
-        do{
-            //Name Request:
-            out.println("######################");
-            out.println("# Hi employee, input your name: ");
-            nameIn=input.nextLine();
+        try {
+            //Begin program loop:
+            do {
+                //Name Request:
+                out.println("######################");
+                out.println("# Hi employee, input your name: ");
+                nameIn = input.nextLine();
 
-            //Hours worked request:
-            out.println("# How many hours did you work? \nEnter here: ");
-            hours = input.nextDouble();
-
-
-            //Wage request:
-            out.println("How much do you make per hour?\nEnter here: ");
-            rate = input.nextDouble();
+                //Hours worked request:
+                out.println("# How many hours did you work? \nEnter here: ");
+                hours = input.nextDouble();
 
 
-            //Begin Calculations:
-            if(hours <=40) {
-                totalPay=hours * rate;
-            } else {
-                totalPay =
-                        (40*rate)+
-                        ((hours-40)*
-                                (rate*1.5));
-            }
+                //Wage request:
+                out.println("How much do you make per hour?\nEnter here: ");
+                rate = input.nextDouble();
 
-            //currency formatting + output
-            out.print(NumberFormat.getCurrencyInstance(new Locale ("en",
-                    "US")).format(totalPay)+ " is your weekly wage.\n# We calculated overtime if you " +
-                    "worked above 40 hours.\n# Have a great week " + nameIn);
-            //Loop query
-            out.println("""
 
-                    Would the next employee like to run the program?
-                    Y = Run again
-                    N = Exit""");
-            loopResponse=input.next();
-            if(loopResponse.equalsIgnoreCase("n")) {
-             out.println("\n\nHave a fantastic day, " +nameIn);
-            } else{continue;}
-        }while(loopResponse.equalsIgnoreCase("Y"));}
+                //Begin Calculations:
+                if (hours <= 40) {
+                    totalPay = hours * rate;
+                } else {
+                    totalPay =
+                            (40 * rate) +
+                                    ((hours - 40) *
+                                            (rate * 1.5));
+                }
+
+                //currency formatting + output
+                out.print(NumberFormat.getCurrencyInstance(new Locale("en",
+                        "US")).format(totalPay) + " is your weekly wage.\n# We calculated overtime if you " +
+                        "worked above 40 hours.\n# Have a great week " + nameIn);
+                //Loop query
+                out.println("Would the next employee like to run the program?\nY = Run again\nN = Exit");
+                loopResponse = input.next();
+                if (loopResponse.equalsIgnoreCase("y")) {
+                    out.println("\n\nHave a fantastic day, " + nameIn + "!");
+                    out.println("Continuing.");
+                }
+                else {
+                    if (loopResponse.equalsIgnoreCase("n")) {
+                        out.println("\n\nThank you for using this application!");
+                        break;}
+                    else {
+                        out.println("\n\n\tImproper response!");
+                        break;
+                    }
+                }
+            } while (loopResponse.equalsIgnoreCase("Y"));
+        }
+        catch (Exception e)
+        {
+            out.println("There was an error running your program!\n" +
+                    "Please try again.");
+        }
+    }
 }
